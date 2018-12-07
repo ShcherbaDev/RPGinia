c3a0f79a787a759159f27c0b3e1cba096f5e5c2b
shcherbaDev <andrey_shcherba@ukr.net>
2018-12-07 18:47:16 +0300

    Level editor: custom interface created

--- level editor/main.js
+++ level editor/main.js
@@ -1,23 +1,32 @@
-const {app, BrowserWindow} = require('electron')
-let mainWindow
+const {app, BrowserWindow} = require('electron');
+const electron = require("electron");
+let mainWindow;
 
-function createWindow () {
-  mainWindow = new BrowserWindow({width: 1200, height: 500})
-  mainWindow.loadFile('index.html')
-  mainWindow.webContents.openDevTools();
-  mainWindow.on('closed', () => { mainWindow = null })
+function createWindow() {
+    const screenSizes = [
+        electron.screen.getPrimaryDisplay().size.width-20,
+        electron.screen.getPrimaryDisplay().size.height-60
+    ];
+
+    mainWindow = new BrowserWindow({
+        width: screenSizes[0], 
+        height: screenSizes[1], 
+        center: true,
+        frame: false
+    })
+    mainWindow.loadFile('index.html');
+    mainWindow.webContents.openDevTools();
+    mainWindow.on('closed', () => { mainWindow = null })
 }
 
 app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
     app.quit()
-  }
 })
 
-app.on('activate', function () {
-  if (mainWindow === null) {
+app.on('activate', function() {
+  if(mainWindow === null) {
     createWindow()
   }
 });
--- level editor/renderer.js
+++ level editor/renderer.js
@@ -1,3 +1,17 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+const {remote, ipcRenderer} = require("electron");
+
+document.querySelector(".custom_frame > .controls > button#minimize").addEventListener("click", (e) => {
+    remote.getCurrentWindow().minimize();
+});
+
+document.querySelector(".custom_frame > .controls > button#resize").addEventListener("click", (e) => {
+    const currentWindow = remote.getCurrentWindow();
+    if(currentWindow.isMaximized())
+        currentWindow.unmaximize();
+    else
+        currentWindow.maximize();
+});
+
+document.querySelector(".custom_frame > .controls > button#close").addEventListener("click", (e) => {
+    remote.app.quit();
+});
